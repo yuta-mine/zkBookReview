7ce29424377632701e1d5e5795f085af2ef07779
yuuta-minede <_jamyukirom@gmail.com>
2023-04-14 23:10:09 +0900

    change ui repo

--- ui/src/pages/nft-list/index.page.tsx
+++ ui/src/pages/nft-list/index.page.tsx
@@ -1,9 +1,9 @@
 
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../../styles/Home.module.css';
 
-export default function Home() {
+export default function NftList() {
   return (
     <div className={styles.container} />
   );
