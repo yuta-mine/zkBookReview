264b6a251ec4121a2c0fd4831416d0d7f7ced6ae
yuuta-minede <_jamyukirom@gmail.com>
2023-04-14 21:54:23 +0900

    fix hardhat config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+	solidity: '0.8.18',
+	networks: {
+		hardhat: {},
+		alfajores: {
+			url: 'https://alfajores-forno.celo-testnet.org',
+			accounts: {
+				mnemonic: process.env.MNEMONIC,
+				path: "m/44'/60'/0'/0",
+			},
+			chainId: 44787,
+		},
+	},
 };
 
 export default config;
