0cb7f064859f505f45de1ea98e8d714e3c7a7ce0
3265 <mgoldchild@gmail.com>
2023-04-15 02:44:49 +0900

    chore: tweak hardhat settings

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -2,18 +2,18 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 require('dotenv').config();
 
+// const { MNEMONIC, INFURA_KEY } = process.env;
+// if (!MNEMONIC) {
+//   throw "must specify MNEMONIC and INFURA_KEY as env variable";
+// }
+
+// const mnWallet = ethers.Wallet.fromMnemonic(MNEMONIC);
+
+
 const config: HardhatUserConfig = {
 	solidity: '0.8.18',
+	defaultNetwork: "hardhat",
 	networks: {
-		hardhat: {},
-		alfajores: {
-			url: 'https://alfajores-forno.celo-testnet.org',
-			accounts: {
-				mnemonic: process.env.MNEMONIC,
-				path: "m/44'/60'/0'/0",
-			},
-			chainId: 44787,
-		},
 	},
 };
 
