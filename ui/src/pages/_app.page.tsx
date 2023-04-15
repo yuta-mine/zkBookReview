c6774127894999c5334c0e2b31c323921900b145
yuuta-minede <_jamyukirom@gmail.com>
2023-04-15 19:45:56 +0900

    introduce chakra ui

--- ui/src/pages/_app.page.tsx
+++ ui/src/pages/_app.page.tsx
@@ -1,8 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import './reactCOIServiceWorker';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
