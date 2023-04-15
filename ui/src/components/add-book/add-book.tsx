195b4321c2ccaf9b7738db13ad46b13f1833d2b3
yuuta-minede <_jamyukirom@gmail.com>
2023-04-15 19:45:24 +0900

    update add book

--- ui/src/components/add-book/add-book.tsx
+++ ui/src/components/add-book/add-book.tsx
@@ -2,12 +2,14 @@ import { FC, useEffect, useState } from 'react';
 import {ethers} from "ethers";
 import MintNFT from "../../abi/MintNFT.json";
 
-export const NFTList: FC = () => {
-  const contractAddress = "0x4fc13E05998A33dFC9067E2A201aCd9C8CaA2405";
+export const AddBook: FC = () => {
+  const contractAddress = "0x1dbb068EF9c4C73F086DBec28aAa6F79CCb5F499";
 
   // -----------------------------------
   const [userAddress, setUserAddress] = useState('');
   const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageTitle, setImageTitle] = useState<string | null>(null);
   const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
 
   useEffect(() => {
@@ -37,12 +39,11 @@ export const NFTList: FC = () => {
       }
     }
     connectWallet()
-    console.log(userAddress)
   }, [userAddress])
 
 
   // add -----------------------------------
-  const addBook = async (title: string) : Promise<void> => {
+  const addBook = async (title: string, description: string, imageId: string) : Promise<void> => {
     try {
       const { ethereum } = window as any;
       if (!ethereum) {
@@ -54,20 +55,30 @@ export const NFTList: FC = () => {
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(contractAddress, MintNFT.abi, signer);
-      const res = await contract.addBook(title);
+      const res = await contract.addBook(title, description, imageId);
       console.log("res: ", res)
     } catch (e) {
       console.log('エラー', e)
     }
   }
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
   };
+  const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+  };
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    setImageTitle(file?.name || '')
+  };
   // -----------------------------------
+
   return <div>
-    <h1>add NFTs</h1>
-    <input type="text" value={title} onChange={handleChange}/>
-      <button onClick={() => addBook(title)}>add</button>
+    <h1>Add NFTs</h1>
+    <input type="text" value={title} onChange={handleTitleChange} />
+    <input type="text" value={description} onChange={handleDescriptionChange} />
+    <input type="file"  onChange={handleImageChange}/>
+    <button onClick={() => addBook(title, description, imageTitle ?? '')}>add</button>
   </div>
 }
---
+++ ui/src/pages/add-book/index.page.tsx
@@ -0,0 +1,9 @@
+
+import styles from '../../styles/Home.module.css';
+import { AddBook as AddBookComponent} from '../../components/add-book'
+
+export default function AddBook() {
+  return (
+    <AddBookComponent />
+  );
+}
