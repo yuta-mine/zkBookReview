b9c6f8cbf5d6a90996b67c9f176079b935a41631
yuuta-minede <_jamyukirom@gmail.com>
2023-04-15 14:37:42 +0900

    fix hardhat config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -14,6 +14,18 @@ const config: HardhatUserConfig = {
 	solidity: '0.8.18',
 	defaultNetwork: "hardhat",
 	networks: {
+		hardhat: {},
+		alfajores: {
+			url: 'https://alfajores-forno.celo-testnet.org',
+			accounts: {
+				mnemonic: process.env.MNEMONIC,
+				path: "m/44'/60'/0'/0",
+			},
+			chainId: 44787,
+		},
+		localhost: {
+			url: 'http://localhost:8545',
+		},
 	},
 };
 
